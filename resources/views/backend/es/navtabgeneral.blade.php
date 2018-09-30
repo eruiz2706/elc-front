4975ffeb04fef62660162bb365d3fe4bcf424a02
eruiz <eruiz2706@gmail.com>
2018-09-30 03:29:55 -0500

    actualizacion tab

--- resources/views/backend/es/navtabgeneral.blade.php
+++ resources/views/backend/es/navtabgeneral.blade.php
@@ -1,6 +1,4 @@
 <?
-
-$active1=(Request::path() == 'es/calendg') ? 'active' : '';
 $active2=(Request::path() == 'es/resultg') ? 'active' : '';
 $active3=(Request::path() == 'es/dicciong') ? 'active' : '';
 $active4=(Request::path() == 'es/preguntfg') ? 'active' : '';
@@ -20,7 +18,7 @@ $active4=(Request::path() == 'es/preguntfg') ? 'active' : '';
 
     <div class="card-body">
       <ul class="nav nav-pills">
-        <li class="nav-item"><a class="nav-link <?=$active1?>" href="{{url('es/calendg')}}">Calendario</a></li>
+
         <li class="nav-item"><a class="nav-link <?=$active2?>" href="{{url('es/resultg')}}">Resultados</a></li>
         <li class="nav-item"><a class="nav-link <?=$active3?>" href="{{url('es/dicciong')}}">Diccionario</a></li>
         <li class="nav-item"><a class="nav-link <?=$active4?>" href="{{url('es/preguntfg')}}">Preguntas frecuentes</a></li>
