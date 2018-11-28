362a48f9900503481ed344ccef73ab92a265ea25
Eduardo Ruiz <eduardoruiz@MacBook-Pro-de-Eduardo.local>
2018-11-28 01:16:16 -0500

    se activo envio de notificaciones a los usuarios

--- resources/views/layouts/adminlte/partials/mainheader.blade.php
+++ resources/views/layouts/adminlte/partials/mainheader.blade.php
@@ -16,7 +16,7 @@
           <i class="fa fa-bell-o" style='padding-top:7px;font-size:24px'></i>
           <span class="badge badge-warning navbar-badge" id='nav_notifi'></span>
         </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="height:450px;overflow-y: auto;">
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
