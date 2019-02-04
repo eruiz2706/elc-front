15fe1f452c6b254082c259a008b06adace133429
eruiz <eruiz2706@gmail.com>
2019-02-03 22:34:59 -0500

    meta policy security

--- resources/views/layouts/adminlte/partials/htmlheader.blade.php
+++ resources/views/layouts/adminlte/partials/htmlheader.blade.php
@@ -1,5 +1,6 @@
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ config('app.name') }} | Dashboard</title>
